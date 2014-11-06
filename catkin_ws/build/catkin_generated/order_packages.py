8f1c2e0d352f9f040c0c86a5d494782ec1912cb7
emJustin Güse/em <emjguese@uos.de/em>
2014-11-06 11:29:16 +0100

    First initialization

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/justin/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/justin/catkin_ws/devel;/opt/ros/hydro".split(';') if "/home/justin/catkin_ws/devel;/opt/ros/hydro" != "" else []
